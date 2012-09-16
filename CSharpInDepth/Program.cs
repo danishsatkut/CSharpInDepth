16d9b61c72ce88995462f594f297248fec0f8269
Danish Aziz Satkut <danish_satkut@hotmail.com>
2012-09-16 21:11:05 +0530

    Implemented querying functionality using Action and Predicate delegates.

--- CSharpInDepth/Program.cs
+++ CSharpInDepth/Program.cs
@@ -11,6 +11,8 @@ namespace CSharpInDepth
         {
             List<Product> products = Product.GetSampleProducts();
 
+            #region Sorting Funtionality
+
             Console.WriteLine("Products sorted by name: ");
             foreach (Product product in products.OrderBy(p => p.Name))
                 Console.WriteLine(product);
@@ -18,6 +20,20 @@ namespace CSharpInDepth
             Console.WriteLine("\nProducts sorted by price: ");
             foreach (Product product in products.OrderBy(p => p.Price))
                 Console.WriteLine(product);
+
+            #endregion
+
+            #region Querying Funtionality
+
+            Console.WriteLine("\nProducts with price greater than 25: ");
+
+            Predicate<Product> test = delegate(Product p) { return p.Price > 25m; };
+            List<Product> matches = products.FindAll(test);
+
+            Action<Product> print = delegate(Product p) { Console.WriteLine(p); };
+            ((List<Product>) matches.OrderBy(p => p.Name)).ForEach(print);
+
+            #endregion
         }
     }
 }
