eeb9e1c1932f03f166355164599485bf040ef732
Danish Aziz Satkut <danish_satkut@hotmail.com>
2012-09-16 21:33:38 +0530

    Implemented the price field as a nullable, used default values in constructor and excluded the Comparators from the products.

--- CSharpInDepth/Product.cs
+++ CSharpInDepth/Product.cs
@@ -13,10 +13,10 @@ namespace CSharpInDepth
             get { return name; }
         }
 
-        readonly decimal price;
-        public decimal Price { get { return price; } }
+        readonly decimal? price;
+        public decimal? Price { get { return price; } }
 
-        public Product(string name, decimal price)
+        public Product(string name, decimal? price = null)
         {
             this.name = name;
             this.price = price;
@@ -33,13 +33,16 @@ namespace CSharpInDepth
                 new Product(name: "Prale Hide and Seek", price: 20m),
                 new Product(name: "Maggie Noodles", price: 30m),
                 new Product(name: "Nescafe Coffee", price: 56m),
-                new Product(name: "C# in Depth", price: 499.99m)
+                new Product(name: "C# in Depth", price: 499.99m),
+                new Product(name: "Silverlight 5 in Action"),
+                new Product(name: "The Great Gatsby")
             };
         }
 
         public override string ToString()
         {
-            return string.Format("{0}: {1}", name, price);
+            string priceString = (price == null ? "Unknown" : price.ToString());
+            return string.Format("{0}: {1}", name, priceString);
         }
     }
 }
